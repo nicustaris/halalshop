145f3006e394b227e146a136a4b78e560dacba8a
nicustaris <nicolae.staris@gmail.com>
2024-01-20 23:56:24 +0000

    update

--- src/layouts/CustomerFavorites/CustomerFavorites.jsx
+++ src/layouts/CustomerFavorites/CustomerFavorites.jsx
@@ -1,10 +1,16 @@
 import "./CustomerFavorites.css";
 
-import steak from "../../assets/steak.png";
 import { useProductsContext } from "../../context/ProductsContext";
+import Cookies from "universal-cookie";
+import { setDoc, doc, updateDoc, arrayUnion, getDoc } from "firebase/firestore";
+import { store } from "../../firebase";
+
+import { toast } from "react-toastify";
 
 const CustomerFavorites = () => {
   const { orders, products } = useProductsContext();
+
+  const cookies = new Cookies();
 
   const allProducts = orders.flatMap((order) => order.order.products);
 
@@ -19,6 +25,51 @@ const CustomerFavorites = () => {
   );
 
   const customerFavorites = sortedProducts.slice(0, 4);
+
+  async function addProduct(name, price, quantity, imageUrl) {
+    let productid = btoa(Math.random()).slice(0, 20);
+
+    if (!cookies.get("cartid")) {
+      cookies.set("cartid", productid, {
+        path: "/",
+        maxAge: 24 * 3600,
+        secure: true,
+        sameSite: "none",
+      });
+
+      await setDoc(doc(store, "usercart", cookies.get("cartid")), {
+        products: [
+          {
+            productName: name,
+            productPrice: price,
+            quantity: parseInt(quantity),
+            imageUrl: imageUrl,
+          },
+        ],
+      });
+    } else {
+      let productRef = doc(store, "usercart", cookies.get("cartid"));
+      const result = await getDoc(productRef);
+
+      let check = result
+        .data()
+        .products.find((element) => element.productName === name);
+      if (check) {
+        toast.warning("Product already added to your cart");
+        setAlert("Product already added to your cart");
+      } else {
+        await updateDoc(productRef, {
+          products: arrayUnion({
+            productName: name,
+            productPrice: price,
+            quantity: parseInt(quantity),
+            imageUrl: imageUrl,
+          }),
+        });
+        toast.success("Product added successfully");
+      }
+    }
+  }
 
   return (
     <section className="align_center customer_favorites">
@@ -39,7 +90,19 @@ const CustomerFavorites = () => {
                     <div className="align_center product_details_footer">
                       <div className="footer_title">£{productInfo.price}</div>
                       <div>
-                        <button className="add_to_cart">Add to basket</button>
+                        <button
+                          className="add_to_cart"
+                          onClick={() =>
+                            addProduct(
+                              productInfo.name,
+                              productInfo.price,
+                              1,
+                              productInfo.imageUrl
+                            )
+                          }
+                        >
+                          Add to basket
+                        </button>
                       </div>
                     </div>
                   </div>
