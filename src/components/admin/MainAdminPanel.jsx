ec14053db0ab331ea4e1d7a1f05c9ba892df7e59
nicustaris <nicolae.staris@gmail.com>
2024-01-25 01:56:28 +0000

    responsiveness adjustment

--- src/components/admin/MainAdminPanel.jsx
+++ src/components/admin/MainAdminPanel.jsx
@@ -16,48 +16,45 @@ const MainAdminPanel = () => {
   return (
     <div>
       <Navbar />
-      <div className="container">
-        <h1>Content Management System (CMS)</h1>
-        <div className="row">
-          <Link to="/Admin/UsersManagement">
-            <div className="box">
-              <FontAwesomeIcon icon={faUser} />
-              Users Management
-            </div>
-          </Link>
-          <Link to="/Admin/ProductsManagement">
-            <div className="box">
-              <FontAwesomeIcon icon={faList} />
-              Products Management
-            </div>
-          </Link>
-        </div>
-        <div className="row">
-          <Link to="/Admin/PromotionManagement">
-            <div className="box">
-              <FontAwesomeIcon icon={faPercentage} />
-              Promotion Products Management
-            </div>
-          </Link>
-          <Link to="/Admin/CategoryManagement">
-            <div className="box">
-              <FontAwesomeIcon icon={faBook} />
-              Category Management
-            </div>
-          </Link>
-          <Link to="/Admin/ShowcaseManagement">
-            <div className="box">
-              <FontAwesomeIcon icon={faCamera} />
-              Showcase Management
-            </div>
-          </Link>
-          <Link to="/Admin/OrdersHistory">
-            <div className="box">
-              <FontAwesomeIcon icon={faBookReader} />
-              Orders History
-            </div>
-          </Link>
-        </div>
+      <h1 className="admin_title">Content Management System (CMS)</h1>
+      <div className="admin_container">
+        <Link to="/Admin/UsersManagement">
+          <div className="box">
+            <FontAwesomeIcon icon={faUser} />
+            Users Management
+          </div>
+        </Link>
+        <Link to="/Admin/ProductsManagement">
+          <div className="box">
+            <FontAwesomeIcon icon={faList} />
+            Products Management
+          </div>
+        </Link>
+
+        <Link to="/Admin/PromotionManagement">
+          <div className="box">
+            <FontAwesomeIcon icon={faPercentage} />
+            Promotion Products Management
+          </div>
+        </Link>
+        <Link to="/Admin/CategoryManagement">
+          <div className="box">
+            <FontAwesomeIcon icon={faBook} />
+            Category Management
+          </div>
+        </Link>
+        <Link to="/Admin/ShowcaseManagement">
+          <div className="box">
+            <FontAwesomeIcon icon={faCamera} />
+            Showcase Management
+          </div>
+        </Link>
+        <Link to="/Admin/OrdersHistory">
+          <div className="box">
+            <FontAwesomeIcon icon={faBookReader} />
+            Orders History
+          </div>
+        </Link>
       </div>
     </div>
   );
